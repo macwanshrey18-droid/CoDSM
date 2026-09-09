b69c539aed7853cd2a46905be0401181e5807a97
shrey1858 <shrey18052008@gmail.com>
2026-09-09 20:02:14 +0530

    Auto-clear stale service worker caches to fix blank white screen on browser and Android

--- public/sw.js
+++ public/sw.js
@@ -1,30 +1,8 @@
-const CACHE_NAME = 'codsm-pwa-v1';
-const urlsToCache = ['/', '/index.html', '/manifest.json', '/favicon.svg'];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
+// Clean Service Worker - No stale file caching to prevent white screen / 404 script bugs
+self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
+  event.waitUntil(self.clients.claim());
 });
